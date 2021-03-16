2d9ea9d1b425d49cf7464546f4725967f86f3aa3
Hemanshu Chauhan <hemanshuchauhan@192.168.1.107>
2021-03-16 01:02:36 +0000

    BDD Java Web Automation framework using Cucumber6 and Junit

---
+++  test-output-thread/report.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "40576cf2-a0cc-404c-8d1d-afff1fc23662",
+    "feature": "This feature file contains the various features of the login page",
+    "scenario": "As a user I want to login with correct credentials",
+    "start": 1615855634975,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1615855645143,
+    "className": "passed"
+  },
+  {
+    "id": "ea13fd65-696e-432c-9417-447d4267146f",
+    "feature": "This feature file contains the account page test cases",
+    "scenario": "Verification of Account Page title",
+    "start": 1615855615492,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1615855627350,
+    "className": "passed"
+  },
+  {
+    "id": "5293de08-5db9-4c9a-9a90-c56739ea5fce",
+    "feature": "This feature file contains the test cases related to Women\u0027s tab",
+    "scenario": "Verify the subcategories",
+    "start": 1615855615496,
+    "group": 17,
+    "content": "",
+    "tags": "@skipscenarioviahook,",
+    "end": 1615855626747,
+    "className": "skipped"
+  },
+  {
+    "id": "50b793bd-4fd6-4de9-b5b1-5de518153827",
+    "feature": "This feature file contains the account page test cases",
+    "scenario": "Verify the Account sections",
+    "start": 1615855627374,
+    "group": 16,
+    "content": "",
+    "tags": "",
+    "end": 1615855636711,
+    "className": "passed"
+  },
+  {
+    "id": "dadf61d4-b037-4644-bd25-e61a5e8c718d",
+    "feature": "This feature file contains the various features of the login page",
+    "scenario": "As a user I want to validate login Page title",
+    "start": 1615855615605,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1615855626808,
+    "className": "passed"
+  },
+  {
+    "id": "551ea48e-ad30-415b-913f-59dc1df0e46e",
+    "feature": "This feature file contains the various features of the login page",
+    "scenario": "As a user I want to validate that forget password link should display",
+    "start": 1615855626894,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1615855634945,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 15,
+    "content": "Thread[pool-1-thread-1,5,main]"
+  },
+  {
+    "id": 16,
+    "content": "Thread[pool-1-thread-2,5,main]"
+  },
+  {
+    "id": 17,
+    "content": "Thread[pool-1-thread-3,5,main]"
+  }
+]);
+});
